83f9b0c23ded5e08331a440ebb8f7f17cd6dde1c
Huỳnh Mai Cao Nhân <huynhnhandanang@gmail.com>
2022-11-17 01:34:25 +0700

    demo login

--- MasterLibrary/MasterLibrary/View/ForgotPass.xaml.cs
+++ MasterLibrary/MasterLibrary/View/ForgotPass.xaml.cs
@@ -21,7 +21,7 @@ namespace MasterLibrary.View
     {
         public ForgotPass()
         {
-            InitializeComponent();
+            //InitializeComponent();
         }
     }
 }
--- MasterLibrary/MasterLibrary/obj/Debug/MasterLibrary_Content.g.i.cs
+++ MasterLibrary/MasterLibrary/obj/Debug/MasterLibrary_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("res/preview.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/images/backgroundloginwindow.jpeg")]
 
 
