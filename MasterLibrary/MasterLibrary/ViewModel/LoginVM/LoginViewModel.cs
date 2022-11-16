fdb2d2e4d604efec4c667d26debad671f54df101
Nhan Huynh <huynhnhandanang@gmail.com>
2022-11-17 00:36:25 +0700

    view login and biding to forgot pass page

---
+++ MasterLibrary/MasterLibrary/ViewModel/LoginVM/LoginViewModel.cs
@@ -0,0 +1,35 @@
+﻿using MasterLibrary.Views.LoginWindow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace MasterLibrary.ViewModel.LoginVM
+{
+    public class LoginViewModel: BaseViewModel
+    {
+        public static Frame MainFrame { get; set; }
+        public ICommand LoadLoginPage { get; set; }
+        public ICommand ForgotPassCM { get; set; }
+
+        public LoginViewModel()
+        {
+            LoadLoginPage = new RelayCommand<Frame>((p) => { return true; }, (p) =>
+            {
+                MainFrame = p;
+                p.Content = new LoginPage();
+            });
+
+            ForgotPassCM = new RelayCommand<Frame>((p) => { return true; }, (p) =>
+            {
+                MainFrame.Content = new ForgotPassPage();
+            });
+        }
+        
+    }
+
+
+}
---
+++ MasterLibrary/MasterLibrary/Views/LoginWindow/LoginPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MasterLibrary.Views.LoginWindow
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void loginbtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void registerbtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+
+    public class NotEmptyValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            return string.IsNullOrWhiteSpace((value ?? "").ToString())
+                ? new ValidationResult(false, "Field is required.")
+                : ValidationResult.ValidResult;
+        }
+    }
+}
