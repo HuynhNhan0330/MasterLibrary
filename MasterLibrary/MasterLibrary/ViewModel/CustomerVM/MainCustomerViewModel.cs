e5c05641ee73b8a0ebd8bd31827a9b6175b066aa
huynhan03 <huynhnhandanang@gmail.com>
2022-11-23 01:12:16 +0700

    Add view/viewmodel of admin and customer

---
+++ MasterLibrary/MasterLibrary/ViewModel/CustomerVM/MainCustomerViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using MasterLibrary.Views.Customer.BuyBookPage;
+using MasterLibrary.Views.Customer.BookLocationPage;
+
+namespace MasterLibrary.ViewModel.CustomerVM
+{
+    public class MainCustomerViewModel
+    {
+        public ICommand FirstLoadML { get; set; }
+        public ICommand LoadBuyBookPageML { get; set; }
+        public ICommand LoadBookLocationPageML { get; set; }
+
+        public MainCustomerViewModel()
+        {
+            LoadBuyBookPageML = new RelayCommand<Frame>((p) => { return true; }, (p) => 
+            {
+                p.Content = new BuyBookPage();
+            });
+
+            LoadBookLocationPageML = new RelayCommand<Frame>((p) => { return true; }, (p) =>
+            {
+                p.Content = new BookLocationPage();
+            });
+        }
+    }
+}
