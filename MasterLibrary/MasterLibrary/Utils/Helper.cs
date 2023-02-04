be3f76ebd52665f792471b74e5cc073505af9ce5
huynhan03 <huynhnhandanang@gmail.com>
2023-02-04 22:54:22 +0700

    Add function to hash password

--- MasterLibrary/MasterLibrary/Utils/Helper.cs
+++ MasterLibrary/MasterLibrary/Utils/Helper.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace MasterLibrary.Utils
 {
@@ -17,6 +15,25 @@ namespace MasterLibrary.Utils
             }
             return String.Format(CultureInfo.InvariantCulture,
                                 "{0:#,#} ₫", money);
+        }
+
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            return Convert.ToBase64String(plainTextBytes);
+        }
+
+        public static string MD5Hash(string input)
+        {
+            StringBuilder hash = new StringBuilder();
+            MD5CryptoServiceProvider md5provider = new MD5CryptoServiceProvider();
+            byte[] bytes = md5provider.ComputeHash(new UTF8Encoding().GetBytes(input));
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                hash.Append(bytes[i].ToString("x2"));
+            }
+            return hash.ToString();
         }
     }
 }
