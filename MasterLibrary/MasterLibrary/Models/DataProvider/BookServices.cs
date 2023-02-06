d84a15cc1cf58fdf024a43c5776515432b514277
Nguyễn Hoàng Minh <109229679+hoangmindrespect@users.noreply.github.com>
2023-02-06 10:16:56 +0700

    Update BookServices.cs

--- MasterLibrary/MasterLibrary/Models/DataProvider/BookServices.cs
+++ MasterLibrary/MasterLibrary/Models/DataProvider/BookServices.cs
@@ -33,7 +33,7 @@ namespace MasterLibrary.Models.DataProvider
                 using (var context = new MasterlibraryEntities())
                 {
                     books = await (from sach in context.SACHes
-                                   join t in context.TANGs on sach.VITRIDAY equals t.MATANG
+                                   join t in context.TANGs on sach.VITRITANG equals t.MATANG
                                    join d in context.DAYKEs on sach.VITRIDAY equals d.MADAY
                                    select new BookDTO
                                    {
